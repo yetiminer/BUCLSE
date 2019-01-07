3164cc1124764e44daca29b412cb68dcdcf51180
henry ashton <github@yetiminer.com>
2019-01-07 16:42:31 +0000

    alter add_order to be able to create multi leg qids

--- UCLSE/exchange.py
+++ UCLSE/exchange.py
@@ -200,147 +200,151 @@ class Orderbook(Orderbook_half):
 
 class Exchange(Orderbook):
 
-        def add_order(self, order, verbose):
-                # add a quote/order to the exchange and update all internal records; return unique i.d.
-                order.qid = self.quote_id
-                self.quote_id = order.qid + 1
-                # if verbose : print('QUID: order.quid=%d self.quote.id=%d' % (order.qid, self.quote_id))
-                tid = order.tid
-                if order.otype == 'Bid':
-                        response=self.bids.book_add(order)
-                        best_price = self.bids.lob_anon[-1][0]
-                        self.bids.best_price = best_price
-                        self.bids.best_tid = self.bids.lob[best_price][1][0][2]
-                else:
-                        response=self.asks.book_add(order)
-                        best_price = self.asks.lob_anon[0][0]
-                        self.asks.best_price = best_price
-                        self.asks.best_tid = self.asks.lob[best_price][1][0][2]
-                return [order.qid, response]
+		def add_order(self, order, verbose,leg=0,qid=None):
+				# add a quote/order to the exchange and update all internal records; return unique i.d.
+				if leg==0:
+					order.qid = self.quote_id
+				else:
+					order.qid=qid+0.000001*leg
+				
+				self.quote_id = self.quote_id + 1
+				# if verbose : print('QUID: order.quid=%d self.quote.id=%d' % (order.qid, self.quote_id))
+				tid = order.tid
+				if order.otype == 'Bid':
+						response=self.bids.book_add(order)
+						best_price = self.bids.lob_anon[-1][0]
+						self.bids.best_price = best_price
+						self.bids.best_tid = self.bids.lob[best_price][1][0][2]
+				else:
+						response=self.asks.book_add(order)
+						best_price = self.asks.lob_anon[0][0]
+						self.asks.best_price = best_price
+						self.asks.best_tid = self.asks.lob[best_price][1][0][2]
+				return [order.qid, response]
 
 
-        def del_order(self, time, order, verbose):
-                # delete a trader's quot/order from the exchange, update all internal records
-                tid = order.tid
-                if order.otype == 'Bid':
-                        self.bids.book_del(order)
-                        if self.bids.n_orders > 0 :
-                                best_price = self.bids.lob_anon[-1][0]
-                                self.bids.best_price = best_price
-                                self.bids.best_tid = self.bids.lob[best_price][1][0][2]
-                        else: # this side of book is empty
-                                self.bids.best_price = None
-                                self.bids.best_tid = None
-                        cancel_record = { 'type': 'Cancel', 'time': time, 'order': order }
-                        self.tape.append(cancel_record)
+		def del_order(self, time, order, verbose):
+				# delete a trader's quot/order from the exchange, update all internal records
+				tid = order.tid
+				if order.otype == 'Bid':
+						self.bids.book_del(order)
+						if self.bids.n_orders > 0 :
+								best_price = self.bids.lob_anon[-1][0]
+								self.bids.best_price = best_price
+								self.bids.best_tid = self.bids.lob[best_price][1][0][2]
+						else: # this side of book is empty
+								self.bids.best_price = None
+								self.bids.best_tid = None
+						cancel_record = { 'type': 'Cancel', 'time': time, 'order': order }
+						self.tape.append(cancel_record)
 
-                elif order.otype == 'Ask':
-                        self.asks.book_del(order)
-                        if self.asks.n_orders > 0 :
-                                best_price = self.asks.lob_anon[0][0]
-                                self.asks.best_price = best_price
-                                self.asks.best_tid = self.asks.lob[best_price][1][0][2]
-                        else: # this side of book is empty
-                                self.asks.best_price = None
-                                self.asks.best_tid = None
-                        cancel_record = { 'type': 'Cancel', 'time': time, 'order': order }
-                        self.tape.append(cancel_record)
-                else:
-                        # neither bid nor ask?
-                        sys.exit('bad order type in del_quote()')
-
-
-
-        def process_order2(self, time, order, verbose):
-                # receive an order and either add it to the relevant LOB (ie treat as limit order)
-                # or if it crosses the best counterparty offer, execute it (treat as a market order)
-                oprice = order.price
-                counterparty = None
-                [qid, response] = self.add_order(order, verbose)  # add it to the order lists -- overwriting any previous order
-                order.qid = qid
-                if verbose :
-                        print('QUID: order.quid=%d' % order.qid)
-                        print('RESPONSE: %s' % response)
-                best_ask = self.asks.best_price
-                best_ask_tid = self.asks.best_tid
-                best_bid = self.bids.best_price
-                best_bid_tid = self.bids.best_tid
-                if order.otype == 'Bid':
-                        if self.asks.n_orders > 0 and best_bid >= best_ask:
-                                # bid lifts the best ask
-                                if verbose: print("Bid $%s lifts best ask" % oprice)
-                                counterparty = best_ask_tid
-                                price = best_ask  # bid crossed ask, so use ask price
-                                if verbose: print('counterparty, price', counterparty, price)
-                                # delete the ask just crossed
-                                self.asks.delete_best()
-                                # delete the bid that was the latest order
-                                self.bids.delete_best()
-                elif order.otype == 'Ask':
-                        if self.bids.n_orders > 0 and best_ask <= best_bid:
-                                # ask hits the best bid
-                                if verbose: print("Ask $%s hits best bid" % oprice)
-                                # remove the best bid
-                                counterparty = best_bid_tid
-                                price = best_bid  # ask crossed bid, so use bid price
-                                if verbose: print('counterparty, price', counterparty, price)
-                                # delete the bid just crossed, from the exchange's records
-                                self.bids.delete_best()
-                                # delete the ask that was the latest order, from the exchange's records
-                                self.asks.delete_best()
-                else:
-                        # we should never get here
-                        sys.exit('process_order() given neither Bid nor Ask')
-                # NB at this point we have deleted the order from the exchange's records
-                # but the two traders concerned still have to be notified
-                if verbose: print('counterparty %s' % counterparty)
-                if counterparty != None:
-                        # process the trade
-                        if verbose: print('>>>>>>>>>>>>>>>>>TRADE t=%5.2f $%d %s %s' % (time, price, counterparty, order.tid))
-                        transaction_record = { 'type': 'Trade',
-                                               'time': time,
-                                               'price': price,
-                                               'party1':counterparty,
-                                               'party2':order.tid,
-                                               'qty': order.qty
-                                              }
-                        self.tape.append(transaction_record)
-                        return transaction_record
-                else:
-                        return None
+				elif order.otype == 'Ask':
+						self.asks.book_del(order)
+						if self.asks.n_orders > 0 :
+								best_price = self.asks.lob_anon[0][0]
+								self.asks.best_price = best_price
+								self.asks.best_tid = self.asks.lob[best_price][1][0][2]
+						else: # this side of book is empty
+								self.asks.best_price = None
+								self.asks.best_tid = None
+						cancel_record = { 'type': 'Cancel', 'time': time, 'order': order }
+						self.tape.append(cancel_record)
+				else:
+						# neither bid nor ask?
+						sys.exit('bad order type in del_quote()')
 
 
 
-        def tape_dump(self, fname, fmode, tmode):
-                dumpfile = open(fname, fmode)
-                for tapeitem in self.tape:
-                        if tapeitem['type'] == 'Trade' :
-                                dumpfile.write('%s, %s\n' % (tapeitem['time'], tapeitem['price']))
-                dumpfile.close()
-                if tmode == 'wipe':
-                        self.tape = []
+		def process_order2(self, time, order, verbose):
+				# receive an order and either add it to the relevant LOB (ie treat as limit order)
+				# or if it crosses the best counterparty offer, execute it (treat as a market order)
+				oprice = order.price
+				counterparty = None
+				[qid, response] = self.add_order(order, verbose)  # add it to the order lists -- overwriting any previous order
+				order.qid = qid
+				if verbose :
+						print('QUID: order.quid=%d' % order.qid)
+						print('RESPONSE: %s' % response)
+				best_ask = self.asks.best_price
+				best_ask_tid = self.asks.best_tid
+				best_bid = self.bids.best_price
+				best_bid_tid = self.bids.best_tid
+				if order.otype == 'Bid':
+						if self.asks.n_orders > 0 and best_bid >= best_ask:
+								# bid lifts the best ask
+								if verbose: print("Bid $%s lifts best ask" % oprice)
+								counterparty = best_ask_tid
+								price = best_ask  # bid crossed ask, so use ask price
+								if verbose: print('counterparty, price', counterparty, price)
+								# delete the ask just crossed
+								self.asks.delete_best()
+								# delete the bid that was the latest order
+								self.bids.delete_best()
+				elif order.otype == 'Ask':
+						if self.bids.n_orders > 0 and best_ask <= best_bid:
+								# ask hits the best bid
+								if verbose: print("Ask $%s hits best bid" % oprice)
+								# remove the best bid
+								counterparty = best_bid_tid
+								price = best_bid  # ask crossed bid, so use bid price
+								if verbose: print('counterparty, price', counterparty, price)
+								# delete the bid just crossed, from the exchange's records
+								self.bids.delete_best()
+								# delete the ask that was the latest order, from the exchange's records
+								self.asks.delete_best()
+				else:
+						# we should never get here
+						sys.exit('process_order() given neither Bid nor Ask')
+				# NB at this point we have deleted the order from the exchange's records
+				# but the two traders concerned still have to be notified
+				if verbose: print('counterparty %s' % counterparty)
+				if counterparty != None:
+						# process the trade
+						if verbose: print('>>>>>>>>>>>>>>>>>TRADE t=%5.2f $%d %s %s' % (time, price, counterparty, order.tid))
+						transaction_record = { 'type': 'Trade',
+											   'time': time,
+											   'price': price,
+											   'party1':counterparty,
+											   'party2':order.tid,
+											   'qty': order.qty
+											  }
+						self.tape.append(transaction_record)
+						return transaction_record
+				else:
+						return None
 
 
-        # this returns the LOB data "published" by the exchange,
-        # i.e., what is accessible to the traders
-        def publish_lob(self, time, verbose):
-                public_data = {}
-                public_data['time'] = time
-                public_data['bids'] = {'best':self.bids.best_price,
-                                     'worst':self.bids.worstprice,
-                                     'n': self.bids.n_orders,
-                                     'lob':self.bids.lob_anon}
-                public_data['asks'] = {'best':self.asks.best_price,
-                                     'worst':self.asks.worstprice,
-                                     'n': self.asks.n_orders,
-                                     'lob':self.asks.lob_anon}
-                public_data['QID'] = self.quote_id
-                public_data['tape'] = self.tape
-                if verbose:
-                        print('publish_lob: t=%d' % time)
-                        print('BID_lob=%s' % public_data['bids']['lob'])
-                        # print('best=%s; worst=%s; n=%s ' % (self.bids.best_price, self.bids.worstprice, self.bids.n_orders))
-                        print('ASK_lob=%s' % public_data['asks']['lob'])
-                        # print('qid=%d' % self.quote_id)
 
-                return public_data
+		def tape_dump(self, fname, fmode, tmode):
+				dumpfile = open(fname, fmode)
+				for tapeitem in self.tape:
+						if tapeitem['type'] == 'Trade' :
+								dumpfile.write('%s, %s\n' % (tapeitem['time'], tapeitem['price']))
+				dumpfile.close()
+				if tmode == 'wipe':
+						self.tape = []
+
+
+		# this returns the LOB data "published" by the exchange,
+		# i.e., what is accessible to the traders
+		def publish_lob(self, time, verbose):
+				public_data = {}
+				public_data['time'] = time
+				public_data['bids'] = {'best':self.bids.best_price,
+									 'worst':self.bids.worstprice,
+									 'n': self.bids.n_orders,
+									 'lob':self.bids.lob_anon}
+				public_data['asks'] = {'best':self.asks.best_price,
+									 'worst':self.asks.worstprice,
+									 'n': self.asks.n_orders,
+									 'lob':self.asks.lob_anon}
+				public_data['QID'] = self.quote_id
+				public_data['tape'] = self.tape
+				if verbose:
+						print('publish_lob: t=%d' % time)
+						print('BID_lob=%s' % public_data['bids']['lob'])
+						# print('best=%s; worst=%s; n=%s ' % (self.bids.best_price, self.bids.worstprice, self.bids.n_orders))
+						print('ASK_lob=%s' % public_data['asks']['lob'])
+						# print('qid=%d' % self.quote_id)
+
+				return public_data
