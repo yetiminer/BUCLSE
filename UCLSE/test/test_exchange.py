1f1be55e5e0efeaf8edd7f16394ba456794b42bf
henry ashton <github@yetiminer.com>
2019-01-10 11:18:49 +0000

    add testing of multi quantity exchange vs single quantity version

--- UCLSE/test/test_exchange.py
+++ UCLSE/test/test_exchange.py
@@ -1,5 +1,8 @@
 from UCLSE.test.utils import yamlLoad,build_lob_from_df,build_df_from_dic_dic,yaml_dump,order_from_dic,pretty_lob_print
+from UCLSE.environment import Market_session, yamlLoad
+from UCLSE.test.utils import identical_replay_vars, side_by_side_period_by_period_difference_checker
 import os
+import copy
 
 def test_lob():
 	cwd=os.getcwd()
@@ -36,3 +39,31 @@ def test_lob():
 		except AssertionError:
 			print('trade record mismatch')
 			raise
+			
+def test_different_process_order_function():
+	pa=os.getcwd()
+	config_name='UCLSE\\test\\fixtures\\mkt_cfg.yml'
+	config_path=os.path.join(pa,config_name)
+	
+	environ_dic=yamlLoad(config_path)
+	environ_dic['end_time']=20
+	
+	sess=Market_session(**environ_dic)
+	sess1=copy.deepcopy(sess)
+	
+	sess.process_order=sess.exchange.process_order3w
+	side_by_side_period_by_period_difference_checker(sess,sess1)
+	
+	assert identical_replay_vars(sess,sess1)=={}
+	assert (sess1.df.fillna(0)==sess.df.fillna(0)).all().all()
+	
+	
+	sess=Market_session(**environ_dic)
+	sess1=copy.deepcopy(sess)
+	
+	sess1.process_order=sess1.exchange.process_order3w
+	
+	side_by_side_period_by_period_difference_checker(sess,sess1)
+	
+	assert identical_replay_vars(sess,sess1)=={}
+	assert (sess1.df.fillna(0)==sess.df.fillna(0)).all().all()
