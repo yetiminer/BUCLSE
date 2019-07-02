dc51309d36ecf7915d1a820270133ca95b57fde7
henry ashton <github@yetiminer.com>
2019-07-02 10:21:40 +0100

    introduce tuples for action choice

--- UCLSE/rl_env.py
+++ UCLSE/rl_env.py
@@ -17,11 +17,13 @@ from collections import OrderedDict
 class RLEnv(gym.Env):
 	metadata = {'render.modes': ['human']}
 
-	def __init__(self,ID='henry',RL_trader=None,inventory_limit=1,time_limit=50,environ_dic=None):
+	def __init__(self,ID='henry',RL_trader=None,inventory_limit=1,time_limit=50,environ_dic=None,thresh=4):
 		
 		self.trader=RL_trader
 		self.environ_dic=environ_dic #this is the config dict for the trading environment
-		self.sess=self.sess_init()
+		self.thresh=thresh
+		self.sess=self.sess_init(thresh)
+		self.assert_exchange()
 		self.ID=ID
 		self.time=self.sess.time
 		self.time_limit=time_limit
@@ -31,8 +33,10 @@ class RLEnv(gym.Env):
 		self.inventory_limit=inventory_limit
 		self.setup_actions()
 		
+		
+		
 	 
-	def __str__(self):
+	def __repr__(self):
 
 		return 'mid price: %d,best_bid: %d, bid_change %d, best_ask %d, ask_change %d, bid ask spread %d'%(self.mid_price,
 																							self.best_bid,self.bid_change,
@@ -79,6 +83,10 @@ class RLEnv(gym.Env):
 	def time_list(self):
 		return list(self.lob_history.keys())
 		
+	def assert_exchange(self):
+		if self.trader.exchange is None:
+			self.trader.set_exchange(self.sess.exchange)
+		
 
 	def side_change(self,side):
 		try:
@@ -100,22 +108,17 @@ class RLEnv(gym.Env):
 	def bid_change(self):
 		return self.side_change('bids')
 
-	def sess_init(self):
+	def sess_init(self,order_thresh=4):
+		#initialise 
 
-
-
-		def geometric_q():
-			return np.random.geometric(0.6)
-		
 		self.environ_dic['rl_traders']={self.trader.tid:self.trader}
 		
 		sess=Market_session(**self.environ_dic)
-		sess.process_order=sess.exchange.process_order3w
-		sess.quantity_f=geometric_q
-
-		order_thresh=4
-		while len(sess.exchange.tape)<2 and min(sess.exchange.bids.n_orders,sess.exchange.asks.n_orders)<order_thresh and sess.timer.next_period():
+		
+		
+		while (len(sess.exchange.tape)<2 or min(sess.exchange.bids.n_orders,sess.exchange.asks.n_orders)<order_thresh) and sess.timer.next_period():
 			sess.simulate_one_period(sess.trade_stats_df3,recording=False)
+			
 		
 		return sess
 
@@ -160,31 +163,59 @@ class RLEnv(gym.Env):
 		
 		return pretty_lob_print(self.sess.exchange)
 
+	# def setup_actions(self):
+		# time=self.time
+		# self.action_dic={
+			# -1: self.cancel_bids, 							#Cancel bid
+			# 1:self.do_order_wrap(time,otype='Bid',spread=-1,qty=1), #add bid at best ask
+			# 10:self.do_order_wrap(time,otype='Bid',spread=0,qty=1), # 'Add bid at best_bid-spread',
+			# 11:self.do_order_wrap(time,otype='Bid',spread=1,qty=1),
+			# 12:self.do_order_wrap(time,otype='Bid',spread=2,qty=1),
+			# 13:self.do_order_wrap(time,otype='Bid',spread=3,qty=1),
+			# 14:self.do_order_wrap(time,otype='Bid',spread=4,qty=1),
+			# 15:self.do_order_wrap(time,otype='Bid',spread=1,qty=1),                   
+			# -2: self.cancel_asks, 							#Cancel ask
+			# 2:self.do_order_wrap(time,otype='Ask',spread=-1,qty=1),#add ask at best bid (hit the bid)
+			# 20:self.do_order_wrap(time,otype='Ask',spread=0,qty=1), # Add ask at best_bid+spread
+			# 21:self.do_order_wrap(time,otype='Ask',spread=1,qty=1),
+			# 22:self.do_order_wrap(time,otype='Ask',spread=2,qty=1),
+			# 23:self.do_order_wrap(time,otype='Ask',spread=3,qty=1),
+			# 24:self.do_order_wrap(time,otype='Ask',spread=4,qty=1),
+			# 25:self.do_order_wrap(time,otype='Ask',spread=1,qty=1),            
+			# 0: self.do_nothing,
+			  # }
+			  
 	def setup_actions(self):
 		time=self.time
 		self.action_dic={
-			-1: self.cancel_bids, 							#Cancel bid
-			1:self.do_order_wrap(time,otype='Bid',spread=-1,qty=1), #add bid at best ask
-			10:self.do_order_wrap(time,otype='Bid',spread=0,qty=1), # 'Add bid at best_bid-spread',
-			11:self.do_order_wrap(time,otype='Bid',spread=1,qty=1),
-			12:self.do_order_wrap(time,otype='Bid',spread=2,qty=1),
-			13:self.do_order_wrap(time,otype='Bid',spread=3,qty=1),
-			14:self.do_order_wrap(time,otype='Bid',spread=4,qty=1),
-			15:self.do_order_wrap(time,otype='Bid',spread=1,qty=1),                   
-			-2: self.cancel_asks, 							#Cancel ask
-			2:self.do_order_wrap(time,otype='Ask',spread=-1,qty=1),#add ask at best bid (hit the bid)
-			20:self.do_order_wrap(time,otype='Ask',spread=0,qty=1), # Add ask at best_bid+spread
-			21:self.do_order_wrap(time,otype='Ask',spread=1,qty=1),
-			22:self.do_order_wrap(time,otype='Ask',spread=2,qty=1),
-			23:self.do_order_wrap(time,otype='Ask',spread=3,qty=1),
-			24:self.do_order_wrap(time,otype='Ask',spread=4,qty=1),
-			25:self.do_order_wrap(time,otype='Ask',spread=1,qty=1),            
-			0: self.do_nothing,
-			  }
+			(0,0,0): Action(otype=None,spread=0,qty=0,trader=self.trader), #do nothing 
+			(1,0,0): Action(otype='Bid',spread=0,qty=0,trader=self.trader),#Cancel bid
+			(1,-1,1):Action(otype='Bid',spread=-1,qty=1,trader=self.trader), #lift best ask
+			(1,0,1):Action(otype='Bid',spread=0,qty=1,trader=self.trader), # 'Add bid at best_bid-spread',
+			(1,1,1):Action(otype='Bid',spread=1,qty=1,trader=self.trader),
+			(1,1,2):Action(otype='Bid',spread=2,qty=1,trader=self.trader),
+			(1,1,3):Action(otype='Bid',spread=3,qty=1,trader=self.trader),
+			(1,1,4):Action(otype='Bid',spread=4,qty=1,trader=self.trader),
+			(1,1,5):Action(otype='Bid',spread=5,qty=1,trader=self.trader),                   
+			(-1,0,0):Action(otype='Ask',spread=0,qty=0,trader=self.trader),#cancel ask
+			(-1,-1,1):Action(otype='Ask',spread=-1,qty=1,trader=self.trader), #hit best bid
+			(-1,0,1):Action(otype='Ask',spread=0,qty=1,trader=self.trader), #add ask at best bid (hit the bid)
+			(-1,1,1):Action(otype='Ask',spread=1,qty=1,trader=self.trader), # Add ask at best_bid+spread
+			(-1,2,1):Action(otype='Ask',spread=2,qty=1,trader=self.trader), 
+			(-1,3,1):Action(otype='Ask',spread=3,qty=1,trader=self.trader),
+			(-1,4,1):Action(otype='Ask',spread=4,qty=1,trader=self.trader),
+			(-1,5,1):Action(otype='Ask',spread=1,qty=1,trader=self.trader), 
+				}			
+					  
+	
 
 	def action_converter(self,action_num):
 	 
-		self.action_dic[action_num](self.lob)
+		new_order=self.action_dic[action_num].do(self.lob)
+		if new_order is not None:
+		
+			#this informs RL_trader of correct qid, does the bookkeeping.
+			self.sess._send_order_to_exchange(self.trader.tid,new_order,trade_stats=None)
 		
 		
 	def stop_checker(self):
@@ -199,15 +230,50 @@ class RLEnv(gym.Env):
 			return True
 		else:    
 			return stop
-
-	def do_nothing(self,lob):
-		pass
-
-	def cancel_order(self,otype='Bid'):
+		
+class Action():
+	def __init__(self,otype=None,spread=0,qty=0,trader=None):
+		self.otype=otype
+		self.spread=spread
+		self.qty=qty
+		try:
+			assert trader is not None
+			self.trader=trader
+		except AssertionError:
+			print('Action must have associated trader on init')
+			raise
+		
+	def __repr__(self):
+		if self.otype is None:
+			ans='Do nothing'
+		elif self.qty==0:
+			ans=f'cancel {self.otype}'
+		else:
+			ans= f' submit or replace {self.otype} with spread {self.spread} and quantity {self.qty}'
+			
+		return ans
+		
+	def do(self,lob):
+		if self.otype is None:
+			#do nothing
+			new_order=None
+		else:
+			self.cancel_order(otype=self.otype)
+		if self.qty==0:
+			#this is purely a cancel
+			new_order=None
+		else:
+			new_order=self.trader.do_order(lob,self.otype,self.spread,self.qty)
+		#send to exchange?
+		return new_order
+	
+	
+	def cancel_order(self,otype):
 		for oi, order in list(self.trader.orders_dic.items()):
 			if order['Original'].otype==otype:
-				self.trader.del_order(oi)
-				self.sess.exchange.del_order(self.time, oid=oi, verbose=True)
+				self.trader.cancel_with_exchange(oid=oi,verbose=False)
+				self.trader.del_order(oi,'cancel',)
+				
 				
 	def cancel_bids(self,lob):
 		self.cancel_order(otype='Bid')
@@ -215,16 +281,4 @@ class RLEnv(gym.Env):
 	def cancel_asks(self,lob):
 		self.cancel_order(otype='Ask')
 		
-	def do_order_wrap(self,time,otype='Ask',spread=0,qty=1):
 		
-		def do_order(lob):
-
-			#withdraw existing order
-			self.cancel_order(otype=otype)
-			#create a new one
-			new_order=self.trader.do_order(self.time,lob,otype,spread,qty)
-			#send to exchange
-
-			self.sess._send_order_to_exchange(self.trader.tid,new_order)
-			
-		return do_order
