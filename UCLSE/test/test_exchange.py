55ea7fa07741c446ce919b6e3030762940b765c3
henry ashton <github@yetiminer.com>
2019-07-02 11:49:57 +0100

    add timer

--- UCLSE/test/test_exchange.py
+++ UCLSE/test/test_exchange.py
@@ -1,6 +1,7 @@
 from UCLSE.test.utils import yamlLoad,build_lob_from_df,build_df_from_dic_dic,yaml_dump,order_from_dic,pretty_lob_print
 from UCLSE.environment import Market_session, yamlLoad
 from UCLSE.test.utils import identical_replay_vars, side_by_side_period_by_period_difference_checker
+from UCLSE.custom_timer import CustomTimer
 import os
 import copy
 from operator import itemgetter
@@ -12,20 +13,23 @@ def test_lob(verbose=False):
 	fixture_list=yamlLoad(fixture_name)
 	
 	for fixture_dic in fixture_list:
+		timer=CustomTimer(start=1,step=9)
 		
 		order_df=build_df_from_dic_dic(fixture_dic['input'])
 		order_df.sort_values(['time','tid'],inplace=True)
 		order_df['oid']=order_df.index.values
 		necessary_cols=['tid','otype','price','qty','time','qid','oid']
 		exchange=build_lob_from_df(order_df,necessary_cols=necessary_cols)
+		exchange.timer=timer
 
 		new_order=order_from_dic(fixture_dic['new_trade'],necessary_cols=necessary_cols)
 		if verbose: print(new_order)
 		exchange.add_order(new_order,verbose=False,qid=new_order.qid)
 		pretty_lob_print(exchange)
-
-		time=10
-		tr, ammended_orders=exchange._process_order(order=new_order,time=time,verbose=False)
+		
+		timer.next_period()
+		
+		tr, ammended_orders=exchange._process_order(order=new_order,time=timer.get_time,verbose=False)
 		
 		
 		try: 
